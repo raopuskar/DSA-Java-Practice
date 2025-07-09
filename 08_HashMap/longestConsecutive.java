a7db53c3d61291cd072b98d4237eb3c78fd9ec38
Puskar rao <raopushkar4@gmail.com>
2025-07-09 14:51:28 +0530

    5th

---
+++ 08_HashMap/longestConsecutive.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class LongestConsecutive {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : numSet) {
+            // Only start counting if it's the start of a sequence
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = longestConsecutive(nums);
+        System.out.println("Length of longest consecutive sequence: " + result);
+    }
+}
