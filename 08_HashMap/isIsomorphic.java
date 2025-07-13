e24f04eeec4ab4447eb915d6e47d653ed0fe29db
Puskar rao <raopushkar4@gmail.com>
2025-07-13 11:13:46 +0530

    8th

--- 08_HashMap/isIsomorphic.java
+++ 08_HashMap/isIsomorphic.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
+//isomoerphic string are throse strings that can be transformed into each other by replacing characters
+//for example: "egg" and "add" are isomorphic, but "foo" and "bar" are not.
+
 public class isIsomorphic {
     public static boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length()) return false;
 
-        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map1 = new HashMap<>();  //here we create two hashmaps to store the mapping of characters
         HashMap<Character, Character> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -16,7 +19,7 @@ public class isIsomorphic {
                 if (map1.get(c1) != c2) return false;
             } else {
                 map1.put(c1, c2);
-            }
+            }                                              //we map both sides because we need to ensure that the mapping is one-to-one and onto so that we can transform one string into another
 
             // Check reverse mapping too
             if (map2.containsKey(c2)) {
