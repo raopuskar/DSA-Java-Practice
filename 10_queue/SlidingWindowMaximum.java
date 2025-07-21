a537c90a8d58e5d8183372c5720be5cba3ff3226
Puskar rao <raopushkar4@gmail.com>
2025-07-21 12:07:06 +0530

    First Commit

--- 10_queue/SlidingWindowMaximum.java
+++ 10_queue/SlidingWindowMaximum.java
@@ -35,3 +35,34 @@ public class SlidingWindowMaximum {
         System.out.println("Output: " + Arrays.toString(res));  // [3, 3, 5, 5, 6, 7]
     }
 }
+
+
+
+//Brute force approach
+
+// import java.util.*;
+// public class BruteForceSlidingWindow {
+//     public static int[] maxSlidingWindow(int[] nums, int k) {
+//         int n = nums.length;
+//         int[] result = new int[n - k + 1];
+
+//         for (int i = 0; i <= n - k; i++) {
+//             int max = nums[i];
+//             for (int j = i; j < i + k; j++) {
+//                 if (nums[j] > max) {
+//                     max = nums[j];
+//                 }
+//             }
+//             result[i] = max;
+//         }
+
+//         return result;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+//         int k = 3;
+//         int[] result = maxSlidingWindow(nums, k);
+//         System.out.println(Arrays.toString(result));
+//     }
+// }
