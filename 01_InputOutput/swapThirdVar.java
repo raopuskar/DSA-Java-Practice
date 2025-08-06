17929533f203d4f05ea14bfa30672d3b29f49a62
Puskar rao <raopushkar4@gmail.com>
2025-08-06 23:47:04 +0530

    First Commit

--- 01_InputOutput/swapThirdVar.java
+++ 01_InputOutput/swapThirdVar.java
@@ -15,3 +15,20 @@ class swapThirdVar {
         
     }
 }
+
+//with third variable
+// class swapThirdVar {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter value of a:");
+    //     int a = sc.nextInt();
+    //     System.out.print("Enter value of b:");
+    //     int b = sc.nextInt();
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
+    //     System.out.print("Value of a is " + a + " and b is " + b);
+    //     sc.close();
+        
+    // }
+//}   
