e806dc42d71799f663284829ea04406e8dec1721
Puskar rao <raopushkar4@gmail.com>
2025-08-12 21:30:54 +0530

    First Commit

---
+++ 02_LoopsConditionals/longestSubarray.java
@@ -0,0 +1,24 @@
+class longestSubarray {
+    public int LongestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int k = 9; // Example target sum
+        int result = LongestSubarray(nums, k);
+        System.out.println("Length of longest subarray with sum " + k + ": " + result);
+    }
+}
