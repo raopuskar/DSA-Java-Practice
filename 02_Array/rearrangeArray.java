1d43f33a517fa81adf87255a4da2b6b7d218c340
Puskar rao <raopushkar4@gmail.com>
2025-08-13 01:30:55 +0530

    First Commit

--- 02_Array/rearrangeArray.java
+++ 02_Array/rearrangeArray.java
@@ -1,21 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class rearrangeArray {
     public static int[] rearrangeArrayy(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
         int pos = 0, neg = 0;
 
-        // Find starting indices for positive and negative numbers
-        while (nums[pos] < 0) pos++;
-        while (nums[neg] >= 0) neg++;
+        // Find all positive and negative numbers
+        List<Integer> positives = new ArrayList<>();
+        List<Integer> negatives = new ArrayList<>();
 
+        for (int num : nums) {
+            if (num > 0) positives.add(num);
+            else negatives.add(num);
+        }
+
+        // Interleave them starting with positive
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                result[i] = nums[pos++];
-                while (pos < n && nums[pos] < 0) pos++;
-            } else {
-                result[i] = nums[neg++];
-                while (neg < n && nums[neg] >= 0) neg++;
-            }
+            result[i] = (i % 2 == 0) ? positives.get(pos++) : negatives.get(neg++);
         }
 
         return result;
