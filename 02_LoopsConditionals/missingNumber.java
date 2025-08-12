4bc44a5865ff74e37478d7e26fcc8622706bca62
Puskar rao <raopushkar4@gmail.com>
2025-08-12 13:37:18 +0530

    First Commit

---
+++ 02_LoopsConditionals/missingNumber.java
@@ -0,0 +1,18 @@
+public class missingNumber {
+    public static int findMissingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2; // Sum of first n natural numbers
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum; // The missing number
+    }
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 4}; // Example input
+        int missingNumber = findMissingNumber(nums);
+        System.out.println("The missing number is: " + missingNumber);
+    }
+}
