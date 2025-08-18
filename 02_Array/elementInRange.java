1289520feaf78522fd1efd3d904b51d1a6aec620
Puskar rao <raopushkar4@gmail.com>
2025-08-18 23:13:01 +0530

    First Commit

---
+++ 02_Array/elementInRange.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+import java.util.HashSet;
+class Solution {
+    boolean check_elements(int arr[], int n, int A, int B) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0;i<n;i++){
+            if(arr[i] >= A && arr[i]<= B){
+                set.add(arr[i]);
+            }
+        }
+        
+        return set.size() == (B-A+1);
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        int A = 1;
+        int B = 5;
+        
+        boolean result = sol.check_elements(arr, n, A, B);
+        System.out.println("All elements from " + A + " to " + B + " are present: " + result);
+    }
+}
