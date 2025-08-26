3e6015facd38139b26cad8f9c776537ba585fd46
Puskar rao <raopushkar4@gmail.com>
2025-08-26 21:43:57 +0530

    First Commit

---
+++ 13_sorting/bubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] bubbleSort(int[] arr) {
+        // code here
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+        
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        sol.bubbleSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
+
