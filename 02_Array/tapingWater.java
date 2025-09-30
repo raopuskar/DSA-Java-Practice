808a62fac3ebec310e8881de09cb231085938d16
Puskar rao <raopushkar4@gmail.com>
2025-09-30 12:33:29 +0530

    First Commit

---
+++ 02_Array/tapingWater.java
@@ -0,0 +1,35 @@
+//Important code
+class Solution {
+    public int trap(int[] height) {
+        int leftMax = 0;
+        int rightMax = 0;
+        int i = 0;
+        int j = height.length - 1;
+        int res = 0;
+        while (i < j) {
+            if (height[i] > leftMax) {
+                leftMax = height[i];
+            }
+            if (height[j] > rightMax) {
+                rightMax = height[j];
+            }
+            if (leftMax <= rightMax) {
+                if (leftMax - height[i] > 0) {
+                    res += leftMax - height[i];
+                }
+                i++;
+            } else {
+                if (rightMax - height[j] > 0) {
+                    res += rightMax - height[j];
+                }
+                j--;
+            }
+        }
+        return res;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(sol.trap(height)); // Output: 6
+    }
+}
