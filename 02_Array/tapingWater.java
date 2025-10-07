e6e6c578d1c5bc93056aa033c2931a71bc830aa6
Puskar rao <raopushkar4@gmail.com>
2025-10-07 22:48:13 +0530

    First Commit

--- 02_Array/tapingWater.java
+++ 02_Array/tapingWater.java
@@ -33,3 +33,8 @@ class Solution {
         System.out.println(sol.trap(height)); // Output: 6
     }
 }
+
+//Example
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
